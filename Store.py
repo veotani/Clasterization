383505c886430670b28de76a21fd28c1b4d3a687
veotani <ion6431@gmail.com>
2016-12-30 22:00:40 +0300

    Refactored Store.py
    Now Store.py is a bunch of functions to be used in a main clasterization
    function

--- Store.py
+++ Store.py
@@ -4,46 +4,51 @@ import re
 import Stemmer
 from io import open
 
-themes = ["science\t", "style\t", "culture\t", "life\t", "economics\t", "business\t", "travel\t", "forces\t",
-          "media\t", "sport\t"]
-infile = "news_train.txt"
-outfile = "news_wo_themes.txt"
-docsize = 60000
 
-TrainTxt = open(infile, 'r', encoding='utf-8')
-OutTxt = open(outfile, "w+", encoding='utf-8')
+def initial_replacements(file_path):
+    train_txt = open(file_path, 'r', encoding='utf-8')
+    doc_list = train_txt.read()
+    yo_caps = "Ё".decode('utf8')
+    ye_caps = "Е".decode('utf8')
+    yo = "ё".decode('utf8')
+    ye = "е".decode('utf8')
+    doc_list = doc_list.replace(yo_caps, ye_caps)
+    doc_list = doc_list.replace(yo, ye)
+    doc_list = doc_list.replace('.', '. ')
+    doc_list = doc_list.replace(';', '; ')
+    doc_list = doc_list.replace(',', ', ')
+    # string
+    return doc_list
 
-DocList = TrainTxt.read()
-YO = "Ё".decode('utf8')
-YE = "Е".decode('utf8')
-yo = "ё".decode('utf8')
-ye = "е".decode('utf8')
-DocList = DocList.replace(YO, YE)
-DocList = DocList.replace(yo, ye)
-DocList = DocList.replace('.', '. ')
-DocList = DocList.replace(';', '; ')
-DocList = DocList.replace(',', ', ')
 
-DocumentsList = []
+def fill_doc_list(docs):
+    documents_list = []
+    docs_splitted = docs.split("\n")
+    docs_splitted.pop(len(docs_splitted)-1)
 
-dclist = DocList.split("\n")
-dclist.pop(60000)
+    for doc in docs_splitted:
+        single_doc = doc.split("\t")
+        new_doc = Dc.Document(single_doc[0], single_doc[1], single_doc[2])
+        documents_list.append(new_doc)
+    # list of Document class objects
+    return documents_list
 
-for doc in dclist:
-    splitted = doc.split("\t")
-    NewDoc = Dc.Document(splitted[0], splitted[1], splitted[2])
-    DocumentsList.append(NewDoc)
 
-dictionary = '[^абвгдежзийклмнопрстуфхцчшщъыьэюя АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-]'.decode('utf8')
-regex = re.compile(dictionary)
-stemmer = Stemmer.Stemmer('russian')
-stemmer.maxCacheSize = 0
+def stem_doc_list(doc_list):
+    dictionary = '[^абвгдежзийклмнопрстуфхцчшщъыьэюя АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-]'.decode('utf8')
+    regex = re.compile(dictionary)
+    stemmer = Stemmer.Stemmer('russian')
+    stemmer.maxCacheSize = 0
 
-for doc in DocumentsList:
-    doc.body = regex.sub('', doc.body.lower())
-    body = ''
-    for word in doc.body.split(' '):
-        word = stemmer.stemWord(word)
-        body = body + word + ' '
-    doc.body = ' '.join(body.split())
-print(DocumentsList[0].body)
+    for doc in doc_list:
+        doc.body = regex.sub('', doc.body.lower())
+        body = ''
+        for word in doc.body.split(' '):
+            word = stemmer.stemWord(word)
+            body = body + word + ' '
+        doc.body = ' '.join(body.split())
+    return doc_list
+
+file_path="news_train.txt"
+docs_ready = stem_doc_list(fill_doc_list(initial_replacements(file_path)))
+print(docs_ready[567].body)
